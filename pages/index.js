848d0786594cf6033e62c7fdb7b910138a7e80ef
pauxiel <paul.scholes50@yahoo.com>
2021-02-01 19:52:15 -0800

    feat: added dark mode

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,48 @@
-import Head from "next/head";
+import Head from 'next/head';
+import Container from '@/components/Container';
 
-export default function Home() {
-  return <>index</>;
+import {
+  useColorMode,
+  Heading,
+  Text,
+  Flex,
+  Stack,
+  Link,
+  Code,
+  Button
+} from '@chakra-ui/react';
+
+export default function Index() {
+  const { colorMode } = useColorMode();
+  const colorSecondary = {
+    light: 'gray.700',
+    dark: 'gray.400'
+  };
+  return (
+    <Container>
+      <Head>
+        <title>Home- Obayuwana Paul</title>
+      </Head>
+
+      <Stack
+        as="main"
+        spacing={8}
+        justifyContent="center"
+        alignItems="flex-start"
+        m="0 auto 4rem auto"
+        maxWidth="700px"
+        px={2}
+      >
+        <Flex
+          flexDirection="column"
+          justifyContent="flex-start"
+          alignItems="flex-start"
+          maxWidth="700px"
+        >
+          <Heading mb={2}>Hi, I'm Obayuwana Paul</Heading>
+          <Text>I am a freelancer, web developer, frontend, backend</Text>
+        </Flex>
+      </Stack>
+    </Container>
+  );
 }
